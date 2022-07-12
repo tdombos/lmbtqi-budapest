589739b45e596e391c0108c9b2f2f9f029d0b27b
Tamás Dombos <tdombos@gmail.com>
2022-07-12 22:00:29 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Iuj_SuRkFqDovqaoTJsFhzDZENiNoFcpM4o172cg294/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDBbE7F5SKTmPjocQmAy9yxrlypojcd1yc';
